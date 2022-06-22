d13770a2f195b9058f918b858789d08b17cb07c6
Roma044 <roma.bakalets0406@gmail.com>
2022-06-22 18:53:15 -0400

    Need To Fix

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  // URI: "mongodb://127.0.0.1/book-store",
+  URI: "mongodb+srv://Roma044:f1vtfTXtehBhghfW@cluster0.ddxmftn.mongodb.net/?retryWrites=true&w=majority",
+};
